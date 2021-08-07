80fda67b2c86dd66ae2e251e45cb215f5e5006ff
Vinicius Holtman <42218598+Vinicius-Holtman@users.noreply.github.com>
2021-08-06 21:52:50 -0300

    Create models

--- database/database.js
+++ database/database.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 // Nome do banco, usuario ex root, e senha do banco
-const connection = new Sequelize('nomeEx', 'root', '3214123', {
+const connection = new Sequelize('nameBank', 'root', '12312412', {
     host: 'localhost',
     dialect: 'mysql'
 })
--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const app = express();
 const bodyParser = require('body-parser')
 const connection = require('./database/database')
 
+const categoriesController = require('./categories/CategoriesController')
+const articlesController = require('./articles/ArticlesController')
+
+const Article = require('./articles/Article')
+const Category = require('./categories/Category')
+
 // View engine
 app.set('view engine', 'ejs')
 
@@ -19,8 +25,12 @@ connection
     .then(() => console.log('Conexao feita com sucesso'))
     .catch((error) => console.log(error))
 
+// Routers
 app.get("/", (req, res) => {
     res.render("index")
 })
+
+app.use("/", categoriesController)
+app.use("/", articlesController)
 
 app.listen(8080, () => console.log("Server is only port 8080"))
