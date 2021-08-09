a3f0c16f90d090fdf9396d558e1cf9a60e773d24
Vinicius Holtman <42218598+Vinicius-Holtman@users.noreply.github.com>
2021-08-09 14:34:38 -0300

    Create new.ejs

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -1,12 +1,25 @@
 const express = require('express');
 const router = express.Router();
+const Category = require('./Category');
+const slugify = require('slugify');
 
-router.get('/categories', (req, res) => {
-    res.send("Rota de categoria")
+router.get("/admin/categories/new", (req, res) => {
+    res.render("admin/categories/new")
 })
 
-router.get('/admin/categories/new', (req, res) => {
-    res.send("Rota para criar uma nova categoria")
+router.post("/categories/save", (req, res) => {
+    var title = req.body.title
+    if(title != undefined) {
+        Category.create({
+            title: title,
+            slug: slugify(title)
+        }).then(() => {
+            res.redirect("/")
+        })
+
+    } else {
+        res.redirect("/admin/categories/new")
+    }
 })
 
 module.exports = router;
--- database/database.js
+++ database/database.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 // Nome do banco, usuario ex root, e senha do banco
-const connection = new Sequelize('nameBank', 'root', '12312412', {
+const connection = new Sequelize('fwefwef', 'root', '123123', {
     host: 'localhost',
     dialect: 'mysql'
 })
