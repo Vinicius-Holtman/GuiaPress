ba4a7c72bcdc8cc5298f24c391460316d7266932
Vinicius Holtman <42218598+Vinicius-Holtman@users.noreply.github.com>
2021-08-12 19:52:59 -0300

    Create option category

--- database/database.js
+++ database/database.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 // Nome do banco, usuario ex root, e senha do banco
-const connection = new Sequelize('dawdasd', 'root', '123123', {
+const connection = new Sequelize('wqfqw', 'root', '123123', {
     host: 'localhost',
     dialect: 'mysql'
 })
